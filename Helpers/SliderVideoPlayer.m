7f734cf6639f163370b8860b2db5254fa2dcda8a
Pho Hale <PhoHale@gmail.com>
2020-03-13 20:40:04 -0400

    minor

--- Helpers/SliderVideoPlayer.m
+++ Helpers/SliderVideoPlayer.m
@@ -1,51 +1,56 @@
 %% Opens a video player with a slider that reflects the current playback position:
 %% Pho Hale, 3/13/2020
 
+global svp;
+
 % svp: SliderVideoPlayer
 svpSettings.sliderWidth = 500;
 svpSettings.sliderHeight = 23;
 svpSettings.sliderX = 10;
 svpSettings.sliderY = 50;
+svpSettings.sliderMajorStep = 0.01;
+svpSettings.sliderMinorStep = 0.1;
 
 % Video Player
 % vidPlayCallbacks.PreFrameUpdate = @(~,~) disp('Pre Frame changed!');
 % vidPlayCallbacks.PostFrameUpdate = @(~,~) disp('Post Frame changed!');
 
 svp.vidPlayer = implay(greyscale_frames, v.FrameRate);
-svp.vidPlayer.addlistener(vidPlayCallbacks.FrameUpdate);
+% svp.vidPlayer.resi
+% svp.vidPlayer.addlistener(vidPlayCallbacks.FrameUpdate);
 
 %% Get the info about the loaded video:
 %vidPlayer.DataSource.Controls.CurrentFrame
-svp.vidInfo.vidPlaySourceType = vidPlayer.DataSource.Type;
+svp.vidInfo.vidPlaySourceType = svp.vidPlayer.DataSource.Type;
 if svp.vidInfo.vidPlaySourceType == "Workspace"
     % Loaded from a workspace variable!
-    svp.vidInfo.vidPlaySourceWorkspaceVariableName = vidPlayer.DataSource.Name;
+    svp.vidInfo.vidPlaySourceWorkspaceVariableName = svp.vidPlayer.DataSource.Name;
     vidPlaySourceWorkspaceVariableValue = eval(vidPlaySourceWorkspaceVariableName);
     svp.vidInfo.numFrames = length(vidPlaySourceWorkspaceVariableValue);
-    svp.vidInfo.currentPlaybackFrame = vidPlayer.DataSource.Controls.CurrentFrame;
+    svp.vidInfo.currentPlaybackFrame = svp.vidPlayer.DataSource.Controls.CurrentFrame;
 else
     % Don't know what to do with videos loaded from disk
     disp("Unhandled type!");
 end
 
 %vidPlayer.viewMenuCallback
-
+% guidata(hObject, handles);
 
 
 %% Slider:
 % PreCallBack = @(~,~) disp('Pause the video here');
 % PostCallBack = @(~,~)disp('Play the video here');
-svp.Figure = figure();
-svp.Slider = uicontrol(svp.Figure,'Style','slider',...
+% svp.Figure = figure();
+
+% svp.Slider = uicontrol(svp.Figure,'Style','slider',...
+svp.Slider = uicontrol(svp.vidPlayer.Parent,'Style','slider',...
                 'Min',0,'Max',svp.vidInfo.numFrames,'Value',1,...
-                'SliderStep',[0.05 0.2],...
+                'SliderStep',[svpSettings.sliderMinorStep svpSettings.sliderMajorStep],...
                 'Position', [svpSettings.sliderX,svpSettings.sliderY,svpSettings.sliderWidth,svpSettings.sliderHeight]);
-addlistener(svp.Slider, 'Value', 'PostSet', slider_post_update_function);
+addlistener(svp.Slider, 'Value', 'PostSet', @slider_post_update_function);
 % addlistener(SliderVideoPlayerSettings.Slider, 'Value', 'PreSet', slider_pre_update_function);
 
-vidPlayer.DataSource.Controls.jumpTo(10);
-
-function output_txt = slider_pre_update_function(~,event_obj)
+function output_txt = slider_pre_update_function(src, event_obj)
     % ~            Currently not used (empty)
     % event_obj    Object containing event data structure
     % Pause the video here:
@@ -53,12 +58,19 @@ function output_txt = slider_pre_update_function(~,event_obj)
     
 end
 
-function output_txt = slider_post_update_function(~,event_obj)
+function output_txt = slider_post_update_function(src, event_obj)
     % ~            Currently not used (empty)
     % event_obj    Object containing event data structure
     % Play the video here:
     % Get the frame from the slider:
-    
+%     updatedFrame = get(event_obj,'NewValue');
+
+    updatedFrame = round(event_obj.AffectedObject.Value);
+
+    %updatedFrame = get(svp.Slider,'Value');
+    disp(updatedFrame);
     % Jump to frame:
-    svp.vidPlayer.DataSource.Controls.jumpTo(10);
+%     h = findobj('Tag','slider1');
+    global svp;
+    svp.vidPlayer.DataSource.Controls.jumpTo(updatedFrame);
 end
